0d21b1b2ea0d6a7cd7d5ab257ff29284a0cd174f
kdjoaolol <77984397+kdjoaolol@users.noreply.github.com>
2021-03-30 22:07:55 -0300

    remocao exercicios e att base_de_dados.py]

--- base_de_dados.py
+++ base_de_dados.py
@@ -1,16 +1,23 @@
-import mysql.connector as mysql
+import pymysql.cursors
 from pprint import pprint
 
-mydb = mysql.connect(
+mydb = pymysql.connect(
   host="localhost",
   user="usuario",
   password="senha",
-  database="base_de_dados"
+  database="base_de_dados",
+  cursorclass=pymysql.cursors.DictCursor
+  
+  
+
 )
-cursor = mydb.cursor()
-cursor.execute("SELECT * FROM users WHERE id BETWEEN 100 AND 120")
+with mydb.cursor() as cursor:
+  cursor.execute("SELECT * FROM users LIMIT 20")
 
-resultados = cursor.fetchall() # quando for usar select
-for x in resultados:
-      pprint(x)
+  resultados = cursor.fetchall() # quando for usar select
+  for x in resultados:
+    pprint(x)
+    print()
 
+
+mydb.close()
